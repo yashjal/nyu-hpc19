30748cad235246b5c57e03513ef75ced51942641
Yash Jalan <yj627@crackle1.cims.nyu.edu>
2019-04-29 22:29:26 -0400

    submit

--- hw5/int_ring.cpp
+++ hw5/int_ring.cpp
@@ -3,31 +3,31 @@
 #include <mpi.h>
 #include <iostream>
 
-double time_pingpong(int Nproc, long Nrepeat, long Nsize, MPI_Comm comm) {
+double time_ring(int Nproc, long Nrepeat, int Nsize, MPI_Comm comm) {
   int rank;
   MPI_Comm_rank(comm, &rank);
 
-  int* msg = (int*) malloc(Nsize);
-  for (long i = 0; i < Nsize; i++) msg[i] = 0;
+  int* msg = (int*) malloc(sizeof(int)*Nsize);
+  for (int i = 0; i < Nsize; i++) msg[i] = 0;
 
   MPI_Barrier(comm);
   double tt = MPI_Wtime();
   for (long repeat  = 0; repeat < Nrepeat; repeat++) {
     MPI_Status status;
-    //for (int node = 0; node < Nproc; node++) {
-      if (rank == 0) {
-        MPI_Send(msg, Nsize, MPI_INT, rank+1, repeat, comm);
-        MPI_Recv(msg, Nsize, MPI_INT, Nproc-1, repeat, comm, &status);
-        for (long i=0; i < Nsize; i++) msg[i] += rank;
-      } else {
-        MPI_Recv(msg, Nsize, MPI_INT, rank-1, repeat, comm, &status);
-        for (long i=0; i < Nsize; i++) msg[i] += rank;
-        MPI_Send(msg, Nsize, MPI_INT, rank+1, repeat, comm);
-      }
-    //}
+    if (rank == 0) {
+      MPI_Send(msg, Nsize, MPI_INT, rank+1, repeat, comm);
+      MPI_Recv(msg, Nsize, MPI_INT, Nproc-1, repeat, comm, &status);
+      for (int i=0; i < Nsize; i++) msg[i] += rank;
+    } else {
+      MPI_Recv(msg, Nsize, MPI_INT, rank-1, repeat, comm, &status);
+      for (int i=0; i < Nsize; i++) msg[i] += rank;
+      MPI_Send(msg, Nsize, MPI_INT, (rank+1)%Nproc, repeat, comm);
+    }
   }
   tt = MPI_Wtime() - tt;
-  for (long i = 0; i < Nsize; i++) printf("total: %d", msg[i]);
+  if (rank == 0) {
+    for (int i = 0; i < Nsize; i++) printf("index: %d, total: %d\n", i, msg[i]);
+  }
   free(msg);
   return tt;
 }
@@ -36,7 +36,7 @@ int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
 
   if (argc < 2) {
-    printf("Usage: mpirun ./int_ring <N>\n");
+    printf("Usage: mpirun ./int_ring <Nrepeat>\n");
     abort();
   }
   long Nrepeat = atoi(argv[1]);
@@ -47,14 +47,10 @@ int main(int argc, char** argv) {
 
   int Nproc;
   MPI_Comm_size(comm, &Nproc);
-  long Nsize = 1;
-  double tt = time_pingpong(Nproc, Nrepeat, Nsize, comm);
-  if (!rank) printf("pingpong latency: %e ms\n", tt/Nrepeat * 1000);
-
-  //Nrepeat = 10000;
-  //long Nsize = 1000000;
-  //tt = time_pingpong(proc0, proc1, Nrepeat, Nsize, comm);
-  //if (!rank) printf("pingpong bandwidth: %e GB/s\n", (Nsize*Nrepeat)/tt/1e9);
+  int Nsize = 500000;
+  double tt = time_ring(Nproc, Nrepeat, Nsize, comm);
+  if (!rank) printf("ring latency: %e ms\n", tt/Nrepeat * 1000/Nproc);
+  if (!rank) printf("ring bandwidth: %e GB/s\n", (Nproc*sizeof(int)*Nsize*Nrepeat)/tt/1e9);
 
   MPI_Finalize();
 }
